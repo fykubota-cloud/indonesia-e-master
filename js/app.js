c45162223e4bef66060c1f822d7d56c0fdfbda48
fykubota-cloud <fy.kubota@gmail.com>
2026-06-18 20:09:54 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Indonesia E Master");
+function showScreen(screenId) {
+  const screens = document.querySelectorAll("section");
+
+  screens.forEach(screen => {
+    screen.style.display = "none";
+  });
+
+  document.getElementById(screenId).style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showScreen("home");
+});
